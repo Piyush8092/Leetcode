3cc63d921fae97ee367e31ad5963b8d80693708b
Piyush Kumar <120687744+Piyush8092@users.noreply.github.com>
2024-08-06 07:09:33 +0530

    Time: 60 ms (93.56%) | Memory: 53.1 MB (37.92%) - LeetSync

---
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.js
@@ -0,0 +1,38 @@
+var reverseKGroup = function(head, k) {
+    var dummy = new ListNode(0);
+        dummy.next = head;
+        var prevGroupTail = dummy;
+
+        while (head) {
+            var groupStart = head;
+            var groupEnd = getGroupEnd(head, k);
+
+            if (!groupEnd)
+                break;
+
+            prevGroupTail.next = reverseList(groupStart, groupEnd.next);
+            prevGroupTail = groupStart;
+            head = prevGroupTail.next;
+        }
+    var newHead = dummy.next;
+    return newHead;
+}
+
+var getGroupEnd = function(head, k) {
+    while (head && k > 1) {
+        head = head.next;
+        k--;
+    }
+    return head;
+}
+
+var reverseList = function(head, stop) {
+    var prev = stop;
+    while (head !== stop) {
+        var next = head.next;
+        head.next = prev;
+        prev = head;
+        head = next;
+    }
+    return prev;
+}
